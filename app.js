6627696698561a5b64ed8b112e5264fffabe187b
Alex Ruan <alexruan2222@gmail.com>
2019-07-04 17:01:23 -0400

    init empty template
    deleted old files
    
    fresh new start as a bootstrap template (rather than from scratch)
    
    app.js edited to allow pages to run from localhost

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = new express();
 const path = require('path');
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static('public'));
 
 app.get('/', function(request, response){
-    response.sendFile(path.join(__dirname+'/public/pages/mainpage.html'));
+    response.sendFile(path.join(__dirname,'index.html'));
 });
 
 app.listen(3000, () => console.log("app started"));
